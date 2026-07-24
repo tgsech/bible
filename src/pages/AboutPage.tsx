3a34b6c16e45e408977d387d1a208d485effd68a
sterisia <jeungyena@gmail.com>
2026-07-24 14:56:48 -0400

    Ain't no way I deployed a typo of all things

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,7 +5,7 @@ export function AboutPage() {
   return (
     <div id="mainBody" className="aboutPage">
       <h1>About</h1>
-      <p>This is a Bibly Typing Game developed with love from the Toronto Good Shepherd Evangelical Church (TGSECH).</p>
+      <p>This is a Bible Typing Game developed with love from the Toronto Good Shepherd Evangelical Church (TGSECH).</p>
       <p>
           Holy Bible, New International Version®, NIV® Copyright ©1973, 1978, 1984, 2011 by Biblica, Inc.® Used by permission. All rights reserved worldwide.
       </p>
