92a03faeaadac9f7ad3b0a6a05a689aa5c66f3a9
sterisia <jeungyena@gmail.com>
2026-07-24 14:53:40 -0400

    Finally left the attribution that was bugging me in the About page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,10 +5,12 @@ export function AboutPage() {
   return (
     <div id="mainBody" className="aboutPage">
       <h1>About</h1>
+      <p>This is a Bibly Typing Game developed with love from the Toronto Good Shepherd Evangelical Church (TGSECH).</p>
       <p>
-        Translation credits and sourcing information for each Bible translation used in this app
-        will go here.
+          Holy Bible, New International Version®, NIV® Copyright ©1973, 1978, 1984, 2011 by Biblica, Inc.® Used by permission. All rights reserved worldwide.
       </p>
+      <p>(Express Licensing via YouVersion API)</p>
+      <p>성경전서 개역한글판 (Korean Revised Version, KRV)</p>
       <Link to="/">← Back home</Link>
     </div>
   );
